e5e40875f368f9a880bf62b58632ef3cf47d91c8
“kishlaychandan00” <kishlaychandan00@email.com>
2024-09-08 21:41:42 +0530

    adding urlshort

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors({ origin: 'https://shorturl0011.netlify.app/' })); 
+app.use(cors()); 
 app.use(express.json()); 
 
 // Routes
