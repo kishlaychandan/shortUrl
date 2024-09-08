9804ea6587f0209239701f772dc44bf0e11add2a
“kishlaychandan00” <kishlaychandan00@email.com>
2024-09-08 18:22:58 +0530

    adding urlshort

--- Backend/Controllers/formController.js
+++ Backend/Controllers/formController.js
@@ -13,7 +13,7 @@ export const shortTheUrl = async (req, res) => {
         const urlObj = await new UrlModel({ originalUrl: url, shortedUrl: uniqueId }).save();
         res.status(200).json({
             message: "shorted link is generated",
-            link: `http://localhost:3000/api/${uniqueId}`
+            link: `https://shorturl-snvl.onrender.com/api/${uniqueId}`
         });
     }
 }
--- FrontendUrlShortner/src/App.jsx
+++ FrontendUrlShortner/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
     try {
       console.log('API call');
-      const response = await axios.post('http://localhost:3000/api/', { url: longUrl });
+      const response = await axios.post('https://shorturl-snvl.onrender.com/api/', { url: longUrl });
       console.log(response.data);
       alert(response.data.message);
       setShortUrl(response.data.link); 
